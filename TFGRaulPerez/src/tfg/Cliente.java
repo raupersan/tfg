79769cffbc21fd92e0ed589f677f5d447580b4d4
unknown <raupersan>
2025-05-03 12:44:19 +0200

    primer commit

---
+++ TFGRaulPerez/src/tfg/Cliente.java
@@ -0,0 +1,31 @@
+package tfg;
+
+public class Cliente {
+    private final int id;
+    private final String nombre;
+    private final String telefono;
+    private final String email;
+
+    public Cliente(int id, String nombre, String telefono, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
