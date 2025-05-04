6960329e7bd5d671bc7b205f45ffeb2722902b64
unknown <raupersan>
2025-05-04 14:46:50 +0200

    commit

---
+++ TFGRaulPerez/src/tfg/Producto.java
@@ -0,0 +1,31 @@
+package tfg;
+
+public class Producto {
+    private final int id;
+    private final String nombre;
+    private final double precio;
+    private final int stock;
+
+    public Producto(int id, String nombre, double precio, int stock) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+}
